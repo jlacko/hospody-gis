878d2115869cc23b11c7b3bb845e7550fff4570c
Jindra Lacko <jindra.lacko@gmail.com>
2019-05-20 23:06:14 +0200

    poisson jako příklad glm

--- code/4-regression.R
+++ code/4-regression.R
@@ -22,6 +22,45 @@ sl_model <- lm(data = grid, barcount ~ pop + beds + stations + vegetation +
 
 print(summary(sl_model))
 
+# poissonův model
+
+poi_model <- glm(data = grid, barcount ~ pop + beds + stations + vegetation, 
+                 family = "poisson")
+
+summary(poi_model)
+
+
+# čtyři varianty reziduálů ----
+
+four_residuals <- data.frame(id = grid$id,
+                             baseline = bl_model$residuals,
+                             simple = model$residuals,
+                             interactive = sl_model$residuals,
+                             poisson = poi_model$residuals)
+
+plot_resids <- ggplot(data = four_residuals) +
+   geom_freqpoly(aes(baseline, color = "gray75"), binwidth = 1) +
+   geom_freqpoly(aes(simple, color = "red"), binwidth = 1) +
+   geom_freqpoly(aes(interactive, color = "goldenrod2"), binwidth = 1) +
+   geom_freqpoly(aes(poisson, color = "cornflowerblue"), binwidth = 1) +
+   scale_color_identity(breaks = c("gray75", # barvy
+                                   "red", 
+                                   "goldenrod2",
+                                   "cornflowerblue"), 
+                        labels = c("baseline", # popisky legendy
+                                   "jednoduchý", 
+                                   "složitější",
+                                   "Poissonův"), 
+                        guide = "legend",
+                        name = "Modely:") + # vynucení zobrazení
+   theme_bw() +
+   theme(axis.title.x = element_blank(),
+         axis.title.y = element_blank(),
+         legend.text.align = 1)
+
+   
+
+# příprava dat pro graf -----
 
 resids <- sl_model$residuals # extract residuals from model
 predikce <- sl_model$fitted.values # předpovědi z modelu
@@ -39,8 +78,8 @@ pal <- colorBin(palette = "RdYlGn",  domain = grid$resids,  bins = 5,  reverse =
 
 grid <- grid %>% # create a HTML formatted popup label of grid cell
    mutate(label = paste0('Predikce ', round(predikce),' hospod, <br>',
-                         'skutečnost ', barcount, ', ', 'rozdíl ',
-                         ifelse(resids > 0, '+', '-'), abs(round(resids)), '.'))
+                         'skutečnost ', barcount, ', ', 'rozdíl <b>',
+                         ifelse(resids > 0, '+', '-'), abs(round(resids)), '</b>.'))
 
 lplot <- leaflet() %>%
    addProviderTiles(providers$CartoDB.Positron) %>%
@@ -50,4 +89,6 @@ lplot <- leaflet() %>%
                fillOpacity = 0.5,
                stroke = F, 
                popup = ~label) 
+
+
 
